b64282635c5d708bb121efd3388b4a0961b8575d
GC_XiaoZhang <zhang15835765326@gmail.com>
2022-08-20 13:44:03 +0800

    Update MongoDB.py

--- MongoDB.py
+++ MongoDB.py
@@ -28,7 +28,7 @@ if now_time_day in delete_day:
     os.system ( 'rm *.json -f' )  # 删除所有json
     print ( data + " 清空备份文件" )
     # os.system ( 'rm backuplog.txt.txt -f' )     # 删除日志
-MONGODB_URI = input('输入MONGODB_URI')
+# MONGODB_URI = input('输入MONGODB_URI')
 commond = 'mongoexport --uri ' + f'{MONGODB_URI}' + ' --collection comment --forceTableScan --type json --out ' + f"{now_time.year}年{now_time.month}月{now_time.day}日.json"
 code = os.system ( commond )
 if code == 0:
